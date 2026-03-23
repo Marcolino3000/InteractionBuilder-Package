1b79f156e6e51eedcd29d065e94fa4bcec23afaf
MarvinWack <marc.uhlherr@gmail.com>
2026-03-23 16:53:16 +0100

    add bootstrapper

---
+++ Runtime/Scripts/SceneManagement/Bootstrapper.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SceneManagement
+{
+    public static class Bootstrapper
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Execute()
+        {
+            Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Global")));
+        }
+    }
+}
